13b4f3303723912f970c41fc918a17e0be427f5f
arvind-mocha <arvindarvind2210@gmail.com>
2021-12-06 12:34:09 +0530

    Initial commit

--- src/one.java
+++ src/one.java
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 // This is from branch main
 
 public class one {
     public static void main(String args[]){
         System.out.println("Branch main");
+=======
+// This is from branch new
+
+public class one {
+    public static void main(String args[]){
+        System.out.println("Branch new");
+>>>>>>> new
     }
 }
