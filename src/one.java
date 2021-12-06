587ab1785513e2df0f62fe2e908292504851f73d
arvind-mocha <arvindarvind2210@gmail.com>
2021-12-06 12:03:25 +0530

    Initial commit

--- src/one.java
+++ src/one.java
@@ -1,5 +1,7 @@
+// This is from branch main
+
 public class one {
     public static void main(String args[]){
-        System.out.println("Branch1");
+        System.out.println("Branch main");
     }
 }
